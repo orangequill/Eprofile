652536abea684d645af9518722a345669d88946b
Sandra Pogarcic <sapogarcic@gmail.com>
2020-03-15 14:58:22 -0400

    separating charts from other js events

--- Scripts/slider.js
+++ Scripts/slider.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
     $(".slider").mouseover(function(){
          $(this).css("background", "#0F1020"); 
     });
@@ -7,60 +6,4 @@ $(document).ready(function(){
     $(".slider").mouseout(function(){
         $(this).css("background", "#d3d3d3"); 
     });
-    
-
-    /* The chart js code was based off of a random walk chart. I borrowed the library because it was the best one I had found. 
-       Source attribution: https://developers.google.com/chart/interactive/docs/gallery/scatterchart#fullhtml
-    */
-    google.charts.load("current", {packages:["corechart"]});
-    google.charts.setOnLoadCallback(drawChart);
-
-    function drawChart() {
-      var data = new google.visualization.DataTable();
-      data.addColumn('number');
-      data.addColumn('number');
-
-      // Central point
-      data.addRow([0, 0]);
-
-      var options = {
-        legend: 'none',
-        colors: ['black'],
-        pointShape: 'circle',
-        pointSize: 15
-      };
-
-      var chart = new google.visualization.ScatterChart(document.getElementById('selectEmotionChart'));
-
-      google.visualization.events.addOneTimeListener(chart, 'ready', initEmotion);
-
-      // Control all other animations by listening to the 'animationfinish' event.
-      google.visualization.events.addListener(chart, 'animationfinish', selectEmotion);
-
-      chart.draw(data, options);
-
-      function initEmotion(){
-        var x = data.getValue(data.getNumberOfRows() - 1, 0);
-        var y = data.getValue(data.getNumberOfRows() - 1, 1);
-        chart.draw(data, options);
-      }      
-
-      function selectEmotion(xVal = 0,yVal= 0){
-        var x = data.getValue(data.getNumberOfRows() - 1, 0);
-        var y = data.getValue(data.getNumberOfRows() - 1, 1);
-        if (xVal != 0){
-          x = xVal;
-        }
-        if (yVal != 0){
-          y = yVal;
-        }        
-        chart.draw(data, options);
-      }      
-
-      function changeEmotion(){
-        var x = $('#xVal :selected').val();
-        var y = $('#yVal :selected').val();
-        selectEmotion(x, y);  
-      };
-    }
 });
