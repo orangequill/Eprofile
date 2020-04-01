9d7d2a193cc1900c3c85852d989ab0dfed7ef314
Sandra Pogarcic <sapogarcic@gmail.com>
2020-04-01 01:04:29 -0400

    adding interactivity with calendar

--- Scripts/calendar.js
+++ Scripts/calendar.js
@@ -1,28 +1,56 @@
 $(document).ready(function(){
-    var date = 1;
-    var newDate = new Date();
-    var year = newDate.getYear();
-    var month = newDate.getMonth();
+    /*Note - var is function scoped and let is block scoped*/
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth();
+    var daysInMonth = new Date(year, month + 1, 0).getDate();
 
-    var i = 0;
-    var calBody = document.getElementById("Calendar").getElementsByTagName('tbody')[0];
+    alert(today);
 
-    for (i; i<6; i++){
-        var row = calBody.insertRow();
-        var cols = 0;
-        for(cols; cols < 7; cols++)
-        {
-            // Insert a cell in the row at index 0
-            var newCell  = row.insertCell(cols);
+    showCalendar(month, year);
 
-            var tmpDate = new Date(year, month, 0);
-            // var num = daysInMonth(month, year);
-            // var dayofweek = tmpDate.getDay();
-
-            // alert('tmpDate');
-            var cellContent = document.createTextNode("1");
-            newCell.appendChild(cellContent);
+    function showCalendar(month, year){
+        let firstDay = new Date(year, month).getDay();
+        let nextMonth = false;
+        var date = 1;
+        var calBody = document.getElementById("Calendar").getElementsByTagName('tbody')[0];
+        
+        // creates elt for each day of each row
+        for (let i = 0; i < 6; i++){
+            var row = calBody.insertRow();
+            for(let cols = 0; cols < 7; cols++)
+            {
+                // Insert a cell in the row at index 0
+                var newCell  = row.insertCell(cols);                
+                let cellContent = null;
+                // alert(i == firstDay);
+                // alert(cols < firstDay);
+                if(i == 0 && cols < firstDay && !nextMonth){
+                    date = 0;
+                    cellContent = document.createTextNode("");             
+                }else if(date > daysInMonth && !nextMonth){
+                    date = 1;
+                    nextMonth = true;
+                    month = month + 1;
+                    cellContent = document.createTextNode(date); 
+                    newCell.setAttribute('id', (month + 1) + '-' + date + '-' + year);            
+                }else{
+                    cellContent = document.createTextNode(date);
+                    newCell.setAttribute('id', (month + 1) + '-' + date + '-' + year);
+                }                
+                newCell.appendChild(cellContent);
+                date++;
+            }
         }
-    }
-    
+    }  
+
+    $('tbody tr td').mouseover(function(){
+        // $(this).css("border", "2px solid #CC66FF");
+        $(this).css("background", "#eaf1f8");
+    });
+
+    $('tbody tr td').mouseout(function(){
+    //    $(this).css("border", "1px solid #ddd");
+       $(this).css("background", "white");
+    });
 });
