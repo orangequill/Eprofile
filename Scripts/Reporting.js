1b35490e88cd3a0a81a9b148850cc382e39ce0e0
Sandra Pogarcic <sapogarcic@gmail.com>
2020-04-04 12:36:30 -0400

    reporting tree

---
+++ Scripts/Reporting.js
@@ -0,0 +1,46 @@
+var treeData = [
+    {
+      "name": "User Profile",
+      "parent": "null",
+      "children": [
+        {
+          "name": "Learning Style",
+          "parent": "User Profile",
+          "children": [
+            {
+              "name": "Style A",
+              "parent": "Learning Style"
+            },
+            {
+              "name": "Style B",
+              "parent": "Learning Style"
+            },
+            {
+              "name": "Style C",
+              "parent": "Learning Style"
+            },
+            {
+              "name": "Style D",
+              "parent": "Learning Style"
+            }
+          ]
+        },
+        {
+          "name": "Emotionality",
+          "parent": "User Profile",
+          "children": [
+            {
+              "name": "Anxiety Score",
+              "parent": "Emotionality"
+            }, 
+            {
+              "name": "Emotionality Score",
+              "parent": "Emotionality"
+            }
+          ]
+        }
+      ]
+    }
+  ];
+
+  
