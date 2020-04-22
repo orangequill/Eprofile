03b434b3b78141ff596b78818483b5aaf8712aa6
Sandra Pogarcic <sapogarcic@gmail.com>
2020-04-22 09:22:01 -0400

    changed to google charts - d3 was too ambitious

--- Assets/Scripts/pie.js
+++ Assets/Scripts/pie.js
@@ -1,45 +1,67 @@
 $(document).ready(function(){
-  var data = [
-    {name: 'self', score: 10, color: 'blue'},
-    {name: 'peer', score: 7, color: 'yellow'},
-    {name: 'educator', score: 9, color: 'red'}
-  ];
+  // var data = [
+  //   {name: 'self', score: 10, color: 'blue'},
+  //   {name: 'peer', score: 7, color: 'yellow'},
+  //   {name: 'educator', score: 9, color: 'red'}
+  // ];
 
-  var totalSum = 42;
+  // var totalSum = 42;
 
-  // set the dimensions and margins of the graph
-  var pieWidth = 200;
-  var pieHeight = 200;
-  // The radius of the pieplot is half the width or half the height (smallest one). 
-  var radius = Math.min(pieWidth, pieHeight) / 2;// - margin
+  // // set the dimensions and margins of the graph
+  // var pieWidth = 200;
+  // var pieHeight = 200;
+  // // The radius of the pieplot is half the width or half the height (smallest one). 
+  // var radius = Math.min(pieWidth, pieHeight) / 2;// - margin
 
-  // append the svg object to the div called 'my_dataviz'
-  var pieSvg = d3.select("#anxietyScoreChart")
-    .append("svg")
-    .attr("width", pieWidth)
-    .attr("height", pieHeight)
-    .append("g")
-    .attr("transform", "translate(" + pieWidth / 2 + "," + pieHeight / 2 + ")");
+  // // append the svg object to the div called 'my_dataviz'
+  // var pieSvg = d3.select("#anxietyScoreChart")
+  //   .append("svg")
+  //   .attr("width", pieWidth)
+  //   .attr("height", pieHeight)
+  //   .append("g")
+  //   .attr("transform", "translate(" + pieWidth / 2 + "," + pieHeight / 2 + ")");
 
-  // Create dummy data
-  var scores = { educator:9, peer:7, self:10 }
-  var labelsOfScores = ["educator", "peer", "self"]
+  // // Create dummy data
+  // var scores = { educator:9, peer:7, self:10 }
+  // var labelsOfScores = ["educator", "peer", "self"]
 
-  // set the color scale
-  var color = d3.scale.ordinal().range(["red", "orange", "blue"]).domain(scores);
+  // // set the color scale
+  // var color = d3.scale.ordinal().range(["red", "orange", "blue"]).domain(scores);
 
-  // Compute the position of each group on the pie:
-  var pie = d3.layout.pie().value(function(d) {return d.value; });
-  var data_ready = pie(d3.entries(scores));
+  // // Compute the position of each group on the pie:
+  // var pie = d3.layout.pie().value(function(d) {return d.value; });
+  // var data_ready = pie(d3.entries(scores));
 
-  var arc = d3.svg.arc().innerRadius(50).outerRadius(radius);
-  //arc.text.attr("transform", function(d) {return "translate(" + labelTest +")";});
+  // var arc = d3.svg.arc().innerRadius(50).outerRadius(radius);
+  // //arc.text.attr("transform", function(d) {return "translate(" + labelTest +")";});
 
 
-  // Build the pie chart
-  pieSvg.selectAll().data(data_ready)
-  .enter().append('path')
-  .attr('d', arc).attr('fill', function(d){ return(color(d.data.key))})
-  //.attr("transform", function(d){ return(d.data.value)})
-  .attr("stroke", "white").style("stroke-width", "2px").style("opacity", 0.7);
+  // // Build the pie chart
+  // pieSvg.selectAll().data(data_ready)
+  // .enter().append('path')
+  // .attr('d', arc).attr('fill', function(d){ return(color(d.data.key))})
+  // //.attr("transform", function(d){ return(d.data.value)})
+  // .attr("stroke", "white").style("stroke-width", "2px").style("opacity", 0.7);
+
+  google.charts.load("current", {packages:["corechart"]});
+  google.charts.setOnLoadCallback(drawChart);
+
+  function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['Survey', 'Score'],
+      ['Self',     10],
+      ['Peer',      7],
+      ['Educator',  9]
+    ]);
+
+    var options = {
+      // is3D: true, // found out you can't combine this and piehole
+      pieHole: 0.4,
+      height:250,
+      width: 400
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('anxietyScoreChart'));
+    chart.draw(data, options);
+  }
 });
--- Assets/Scripts/quadTime.js
+++ Assets/Scripts/quadTime.js
@@ -5,131 +5,169 @@ function strToDate(str) {
 
 
 $(document).ready(function(){
-    d3.csv("./Resources/quadrantData.csv", function(d) {
-        d.forEach(function(vals) {
-          date = strToDate(vals.date);
-          quad = vals.quadrant;
-          X = +vals.X;
-          Y = +vals.Y;
-          Magnitude = +vals.Magnitude;
-        });
+    // d3.csv("./Resources/quadrantData.csv", function(d) {
+    //     d.forEach(function(vals) {
+    //       date = strToDate(vals.date);
+    //       quad = vals.quadrant;
+    //       X = +vals.X;
+    //       Y = +vals.Y;
+    //       Magnitude = +vals.Magnitude;
+    //     });
         
-        var width = 400 - 20 - 20;
-        var height = 200 - 20 - 20;
+    //     var width = 400 - 20 - 20;
+    //     var height = 200 - 20 - 20;
 
-        var dataXrange = d3.extent(d, function(vals) { return vals.date; });
-        var dataYrange = [0,d3.max(d, function(vals) { return vals.Magnitude; })];
+    //     var dataXrange = d3.extent(d, function(vals) { return vals.date; });
+    //     var dataYrange = [0,d3.max(d, function(vals) { return vals.Magnitude; })];
 
-        // axes setup
-        var xScale = d3.time.scale()
-        .clamp(true)
-        .range([10, width - 15])
-        .domain(dataXrange);
+    //     // axes setup
+    //     var xScale = d3.time.scale()
+    //     .clamp(true)
+    //     .range([10, width - 15])
+    //     .domain(dataXrange);
 
-        var yScale = d3.scale.linear()
-        .clamp(true)
-        .range([height, 0])
-        .domain(dataYrange);
+    //     var yScale = d3.scale.linear()
+    //     .clamp(true)
+    //     .range([height, 0])
+    //     .domain(dataYrange);
 
-        var xAxis = d3.svg.axis()
-            .scale(xScale)
-            .ticks(16)
-            .tickFormat(d3.time.format("%b %d"))
-            .tickPadding(-2)
-            .tickSize(10)
-            .orient("bottom");
+    //     var xAxis = d3.svg.axis()
+    //         .scale(xScale)
+    //         .ticks(16)
+    //         .tickFormat(d3.time.format("%b %d"))
+    //         .tickPadding(-2)
+    //         .tickSize(10)
+    //         .orient("bottom");
 
-        var yAxis = d3.svg.axis()
-            .scale(yScale)
-            .orient("left")
-            .tickSize(10)
-            .ticks(10);
+    //     var yAxis = d3.svg.axis()
+    //         .scale(yScale)
+    //         .orient("left")
+    //         .tickSize(10)
+    //         .ticks(10);
 
-        var svg = d3.select("#emotionalityScoreChart")
-            .append("svg")
-            .attr("width", width + 20 + 20)
-            .attr("height", height + 20 + 20)
-            .append("g")
-             .attr("preserveAspectRatio", "xMinYMin meet")
-             .classed("svg-content", true)
-            .attr("transform", "translate(" + 40 + "," + 10 + ")");
+    //     var svg = d3.select("#emotionalityScoreChart")
+    //         .append("svg")
+    //         .attr("width", width + 20 + 20)
+    //         .attr("height", height + 20 + 20)
+    //         .append("g")
+    //          .attr("preserveAspectRatio", "xMinYMin meet")
+    //          .classed("svg-content", true)
+    //         .attr("transform", "translate(" + 40 + "," + 10 + ")");
         
-        svg.append("g")            
-            .classed("x axis", true)
-            .attr("transform", "translate(-10," + height + ")")
-            .attr("fill", "none")
-            .attr("stroke", "black")
-            .attr("stroke-opacity", 1)
-            .attr("stroke-width", 1)
-            .call(xAxis);
+    //     svg.append("g")            
+    //         .classed("x axis", true)
+    //         .attr("transform", "translate(-10," + height + ")")
+    //         .attr("fill", "none")
+    //         .attr("stroke", "black")
+    //         .attr("stroke-opacity", 1)
+    //         .attr("stroke-width", 1)
+    //         .call(xAxis);
     
-        svg.append("g")
-            .classed("y axis", true)
-            .attr("fill", "none")
-            .attr("stroke", "black")
-            .attr("stroke-opacity", 1)
-            .attr("stroke-width", 1)
-            .call(yAxis);
+    //     svg.append("g")
+    //         .classed("y axis", true)
+    //         .attr("fill", "none")
+    //         .attr("stroke", "black")
+    //         .attr("stroke-opacity", 1)
+    //         .attr("stroke-width", 1)
+    //         .call(yAxis);
 
-        var q1 = $(d).filter(function (i,n){
-            return n.quadrant==='q1';
-        });
+    //     var q1 = $(d).filter(function (i,n){
+    //         return n.quadrant==='q1';
+    //     });
 
-        var q2 = $(d).filter(function (i,n){
-            return n.quadrant==='q2';
-        });
+    //     var q2 = $(d).filter(function (i,n){
+    //         return n.quadrant==='q2';
+    //     });
 
-        var q3 = $(d).filter(function (i,n){
-            return n.quadrant==='q3';
-        });
+    //     var q3 = $(d).filter(function (i,n){
+    //         return n.quadrant==='q3';
+    //     });
 
-        var q4 = $(d).filter(function (i,n){
-            return n.quadrant==='q4';
-        });
+    //     var q4 = $(d).filter(function (i,n){
+    //         return n.quadrant==='q4';
+    //     });
 
-        var quad1Line = d3.svg.line()
-        .interpolate("basis")
-        .x(function(q1) { return xScale(q1.date); })
-        .y(function(q1) { return yScale(q1.Magnitude); });
+    //     var quad1Line = d3.svg.line()
+    //     .interpolate("basis")
+    //     .x(function(q1) { return xScale(q1.date); })
+    //     .y(function(q1) { return yScale(q1.Magnitude); });
 
-        var quad2Line = d3.svg.line()
-        .interpolate("basis")    
-        .x(function(q2) { return xScale(q2.date); })
-        .y(function(q2) { return yScale(q2.Magnitude); });
+    //     var quad2Line = d3.svg.line()
+    //     .interpolate("basis")    
+    //     .x(function(q2) { return xScale(q2.date); })
+    //     .y(function(q2) { return yScale(q2.Magnitude); });
 
-        var quad3Line = d3.svg.line()
-        .interpolate("basis")
-        .x(function(q3) { return xScale(q3.date); })
-        .y(function(q3) { return yScale(q3.Magnitude); });
+    //     var quad3Line = d3.svg.line()
+    //     .interpolate("basis")
+    //     .x(function(q3) { return xScale(q3.date); })
+    //     .y(function(q3) { return yScale(q3.Magnitude); });
 
-        var quad4Line = d3.svg.line()
-        .interpolate("basis")
-        .x(function(q4) { return xScale(q4.date); })
-        .y(function(q4) { return yScale(q4.Magnitude); });
+    //     var quad4Line = d3.svg.line()
+    //     .interpolate("basis")
+    //     .x(function(q4) { return xScale(q4.date); })
+    //     .y(function(q4) { return yScale(q4.Magnitude); });
 
-        svg.append("path")        
-            .style("stroke", "blue")
-            .attr("d", quad1Line(q1))
-            .attr("stroke-width", 2)
-            .attr("fill", "none");
+    //     svg.append("path")        
+    //         .style("stroke", "blue")
+    //         .attr("d", quad1Line(q1))
+    //         .attr("stroke-width", 2)
+    //         .attr("fill", "none");
 
-        svg.append("path")        
-            .style("stroke", "red")
-            .attr("d", quad2Line(q2))
-            .attr("stroke-width", 2)
-            .attr("fill", "none");
+    //     svg.append("path")        
+    //         .style("stroke", "red")
+    //         .attr("d", quad2Line(q2))
+    //         .attr("stroke-width", 2)
+    //         .attr("fill", "none");
 
-        svg.append("path")        
-            .style("stroke", "purple")
-            .attr("d", quad3Line(q3))
-            .attr("stroke-width", 2)
-            .attr("fill", "none");
+    //     svg.append("path")        
+    //         .style("stroke", "purple")
+    //         .attr("d", quad3Line(q3))
+    //         .attr("stroke-width", 2)
+    //         .attr("fill", "none");
 
-        svg.append("path")        
-            .style("stroke", "green")
-            .attr("d", quad4Line(q4))
-            .attr("stroke-width", 2)
-            .attr("fill", "none");
+    //     svg.append("path")        
+    //         .style("stroke", "green")
+    //         .attr("d", quad4Line(q4))
+    //         .attr("stroke-width", 2)
+    //         .attr("fill", "none");
             
-    });
+    // });
+    // Me figuring out d3 will need to be a later thing. I definitely still want to, but it keeps giving me NANs, though I can parse the data???
+    // But google charts is a solid alternative and a billion times easier. That being said, d3 is still the chosen alternative if this scales.
+
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Month-Day', 'Q1', 'Q2', 'Q3', 'Q4'],
+            ['1-1',0,0,0,1.41],
+            ['1-3',0,0,2.23,0],
+            ['1-10',0,0,0,3.16],
+            ['1-15',0,2.23,0,0],
+            ['1-18',2.83,0,0,0],
+            ['1-20',0,0,5.0,0],
+            ['1-29',5.83,0,0,0],
+            ['1-30',0,5.0,0,0],
+            ['2-1',0,0,0,2.83],
+            ['2-3',0,2.83,0,0],
+            ['2-8',7.07,0,0,0],
+            ['2-10',0,0,2.83,0],
+            ['2-22',0,5.38,0,0],
+            ['2-23',0,0,0,5.83],
+            ['2-26',5.38,0,0,0],
+            ['2-27',0,0,5.38,0]
+        ]);
+
+        var options = {
+          title: 'Quadrant Magnitude Over Time',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('emotionalityScoreChart'));
+
+        chart.draw(data, options);
+    }
+
   });
